a8ee0c99e1d57ed60ebc5b182601a22337f62bfb
Sodja Nathan <sodjanathan@gmail.com>
2022-05-10 18:11:30 +0000

    fetching todos

--- force-app/main/default/lwc/todoManager/todoManager.js
+++ force-app/main/default/lwc/todoManager/todoManager.js
@@ -1,5 +1,7 @@
 import { LightningElement, track } from "lwc";
-import addTodo from "@salesforce/apex/ToDoCobtroller.addTodo";
+import addTodo from "@salesforce/apex/ToDoController.addTodo";
+import getCurrentToDos from "@salesforce/apex/ToDoController.getCurrentToDos";
+
 export default class TodoManager extends LightningElement {
   // track makes the value reactive
   @track time = "8:15 AM";
@@ -9,11 +11,10 @@ export default class TodoManager extends LightningElement {
   // this is like useEffect
   // basically means when component mounts to DOM, do this!
   connectedCallback() {
-    // setInterval(() => {
     this.getTime();
     this.setGreeting(new Date().getHours());
-    this.populateTodos();
-    // }, 1000 * 60);
+    // this.populateTodos();
+    this.fetchTodos();
   }
 
   getTime() {
@@ -48,6 +49,19 @@ export default class TodoManager extends LightningElement {
     }
   }
 
+  // fetch TODOs
+  fetchTodos() {
+    getCurrentToDos()
+      .then((response) => {
+        this.todos = response;
+        console.log("response", response);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }
+
+  // add TODO
   addTodoHandler() {
     const inputBox = this.template.querySelector("lightning-input");
 
@@ -60,6 +74,7 @@ export default class TodoManager extends LightningElement {
       addTodo({ payload: JSON.stringify(todo) })
         .then((response) => {
           console.log("Item inserted successfully", response);
+          this.fetchTodos();
         })
         .catch((error) => {
           console.log("error", error);
